5a91292451f8b3e99cac0fa0d32a14769dbcfe9d
Guy Balzam <guybal@gmail.com>
2017-10-19 16:33:46 +0300

    react 16 support

--- dist-modules/Tag.js
+++ dist-modules/Tag.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 var React = require('react');
 
 var _require = require('react-dnd');
@@ -8,6 +16,7 @@ var DragSource = _require.DragSource;
 var DropTarget = _require.DropTarget;
 
 var flow = require('lodash/function/flow');
+var PropTypes = require('prop-types');
 
 var ItemTypes = { TAG: 'tag' };
 
@@ -39,40 +48,52 @@ function dropCollect(connect, monitor) {
     };
 }
 
-var Tag = React.createClass({
-    displayName: 'Tag',
+var Tag = (function (_React$Component) {
+    _inherits(Tag, _React$Component);
 
-    propTypes: {
-        labelField: React.PropTypes.string,
-        onDelete: React.PropTypes.func.isRequired,
-        tag: React.PropTypes.object.isRequired,
-        moveTag: React.PropTypes.func.isRequired
-    },
-    getDefaultProps: function getDefaultProps() {
-        return {
-            labelField: 'text'
-        };
-    },
-    render: function render() {
-        var label = this.props.tag[this.props.labelField];
-        var _props = this.props;
-        var connectDragSource = _props.connectDragSource;
-        var isDragging = _props.isDragging;
-        var connectDropTarget = _props.connectDropTarget;
+    function Tag() {
+        _classCallCheck(this, Tag);
 
-        return connectDragSource(connectDropTarget(React.createElement(
-            'span',
-            { style: { opacity: isDragging ? 0 : 1 },
-                className: 'ReactTags__tag' },
-            label,
-            React.createElement(
-                'a',
-                { className: 'ReactTags__remove',
-                    onClick: this.props.onDelete },
-                '×'
-            )
-        )));
+        _get(Object.getPrototypeOf(Tag.prototype), 'constructor', this).apply(this, arguments);
     }
-});
 
-module.exports = flow(DragSource(ItemTypes.TAG, tagSource, dragCollect), DropTarget(ItemTypes.TAG, tagTarget, dropCollect))(Tag);
+    _createClass(Tag, [{
+        key: 'getDefaultProps',
+        value: function getDefaultProps() {
+            return {
+                labelField: 'text'
+            };
+        }
+    }, {
+        key: 'render',
+        value: function render() {
+            var label = this.props.tag[this.props.labelField];
+            var _props = this.props;
+            var connectDragSource = _props.connectDragSource;
+            var isDragging = _props.isDragging;
+            var connectDropTarget = _props.connectDropTarget;
+
+            return connectDragSource(connectDropTarget(React.createElement(
+                'span',
+                { style: { opacity: isDragging ? 0 : 1 },
+                    className: 'ReactTags__tag' },
+                label,
+                React.createElement(
+                    'a',
+                    { className: 'ReactTags__remove',
+                        onClick: this.props.onDelete },
+                    '×'
+                )
+            )));
+        }
+    }]);
+
+    return Tag;
+})(React.Component);
+
+Tag.propTypes = {
+    labelField: PropTypes.string,
+    onDelete: PropTypes.func.isRequired,
+    tag: PropTypes.object.isRequired,
+    moveTag: PropTypes.func.isRequired
+}, module.exports = flow(DragSource(ItemTypes.TAG, tagSource, dragCollect), DropTarget(ItemTypes.TAG, tagTarget, dropCollect))(Tag);
